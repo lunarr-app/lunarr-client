105f00179d431f863f6e5613dedb30892af3931b
Sayem Chowdhury <sayem314@gmail.com>
2023-06-09 22:55:04 +0600

    design login page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,60 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { useAuth } from "../../context/auth";
+import { useRouter } from "expo-router";
 
 const SignIn: React.FC = () => {
   const { signIn } = useAuth();
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    signIn();
+    // perform any additional login logic here
+  };
+
+  const handleSignup = () => {
+    router.push("/sign-up");
+  };
 
   return (
     <View style={styles.container}>
-      <Text onPress={() => signIn()}>Sign In</Text>
+      <Text style={styles.title}>Welcome to Lunarr</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.signupText}>
+        Don't have an account?{" "}
+        <Text style={styles.signupLink} onPress={handleSignup}>
+          Signup
+        </Text>
+      </Text>
     </View>
   );
 };
@@ -16,6 +64,48 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 40,
+  },
+  form: {
+    width: "80%",
+    alignItems: "center",
+    maxWidth: 400, // maximum width for the login container
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: "#2196F3",
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  signupText: {
+    fontSize: 14,
+    color: "#333",
+  },
+  signupLink: {
+    color: "#2196F3",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
   },
 });
 
