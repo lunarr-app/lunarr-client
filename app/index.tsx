100186a0a8cc1bfff85d3b573aa8b87f1d5fec33
Sayem Chowdhury <sayem314@gmail.com>
2023-06-10 13:56:08 +0600

    update homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,95 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  View,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
 import { useAuth } from "../context/auth";
 
-const Index: React.FC = () => {
+const HomePage: React.FC = () => {
   const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleLogin = () => {
+    signOut();
+    router.push("/login");
+  };
+
+  const handleSignup = () => {
+    signOut();
+    router.push("/signup");
+  };
 
   return (
-    <View style={styles.container}>
-      <Text onPress={() => signOut()}>Sign Out</Text>
-    </View>
+    <ImageBackground
+      source={require("../assets/images/sigma-5.gif")}
+      style={styles.backgroundImage}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Welcome to Lunarr!</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.signupButton]}
+          onPress={handleSignup}
+        >
+          <Text style={[styles.buttonText, styles.signupButtonText]}>
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    marginVertical: 40,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#fff", // White text color
+  },
+  button: {
+    width: 220,
+    height: 50,
+    backgroundColor: "#3b5998",
+    borderRadius: 25,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 12,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+  signupButton: {
+    backgroundColor: "#ffffff",
+    borderWidth: 2,
+    borderColor: "#3b5998",
+  },
+  signupButtonText: {
+    color: "#3b5998",
   },
 });
 
-export default Index;
+export default HomePage;
