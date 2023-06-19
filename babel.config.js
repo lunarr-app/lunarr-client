8460c0a4a8d9de49b66f95caa2c65b2f8124ab31
Sayem Chowdhury <sayem314@gmail.com>
2023-06-19 23:30:46 +0600

    plugin module-resolver

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,15 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@backend": "./backend",
+          },
+        },
+      ],
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
