d2a437012996d38ce930b2375ac0c2077ed24a56
Sayem Chowdhury <sayem314@gmail.com>
2023-06-20 15:52:42 +0600

    set dark theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,13 @@
 import { Slot } from "expo-router";
 import { useProtectedRoute } from "@store/auth";
+import { ThemeProvider, DarkTheme } from "@react-navigation/native";
 
 export default function Root() {
   useProtectedRoute();
 
-  return <Slot />;
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
