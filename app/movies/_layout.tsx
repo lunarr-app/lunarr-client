f0c57b16027b30a55dc5c359c48bc254d5bbe091
Sayem Chowdhury <sayem314@gmail.com>
2023-06-20 16:07:41 +0600

    setup drawer

--- app/movies/_layout.tsx
+++ app/movies/_layout.tsx
@@ -1,10 +1,39 @@
+import { StyleSheet } from "react-native";
+import { useTheme } from "@react-navigation/native";
 import { createDrawerNavigator, DrawerNavigationOptions } from "@react-navigation/drawer";
 import { withLayoutContext } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const { Navigator } = createDrawerNavigator();
 
 const Drawer = withLayoutContext<DrawerNavigationOptions, typeof Navigator>(Navigator);
 
 export default function Layout() {
-  return <Drawer />;
+  const theme = useTheme();
+
+  return (
+    <Drawer
+      screenOptions={({ navigation }) => ({
+        headerLeft: () => (
+          <MaterialCommunityIcons
+            name="menu"
+            size={24}
+            color={theme.colors.text}
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+            style={styles.menuIcon}
+          />
+        ),
+        gestureEnabled: true,
+        drawerType: "slide",
+      })}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  menuIcon: {
+    marginLeft: 10,
+  },
+});
