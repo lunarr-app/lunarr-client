5cf308c0baa63e751a660b8cf7f7673ca9f502a0
Sayem Chowdhury <sayem314@gmail.com>
2023-06-20 22:18:00 +0600

    set custom theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,23 @@ import { Slot } from "expo-router";
 import { useProtectedRoute } from "@store/auth";
 import { ThemeProvider, DarkTheme } from "@react-navigation/native";
 
+// Customize the DarkTheme
+const CustomDarkTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    background: "#1f1f1f", // Dark background color
+    card: "#252525", // Dark card background color
+    text: "#ffffff", // White text color
+    border: "#999999", // Gray border color
+  },
+};
+
 export default function Root() {
   useProtectedRoute();
 
   return (
-    <ThemeProvider value={DarkTheme}>
+    <ThemeProvider value={CustomDarkTheme}>
       <Slot />
     </ThemeProvider>
   );
